b0bf9f1c1b5df16457773621649df2c0029cf555
hoge1e3 <hoge1e3@gmail.com>
2021-04-04 21:37:12 +0900

    tokenFactory

---
+++ src/lang/tonyu1_token.js
@@ -0,0 +1,29 @@
+/*define(["Grammar", "XMLBuffer", "IndentBuffer","disp", "Parser","TError"],
+function (Grammar, XMLBuffer, IndentBuffer, disp, Parser,TError) {
+*/
+const tokenizerFactory=require("./tokenizerFactory");
+
+module.exports=tokenizerFactory({
+	reserved:{
+		'while':true,
+        'switch':true,
+        'case':true,
+        'default':true,
+        'break':true,
+        'if':true,
+        'is':true,
+        'in':true,
+        'else':true,
+        'null':true,
+        'for':true,
+        'fork':true,
+        'function':true,
+        'constructor':true,
+        'destructor':true,
+        'extends':true,
+        'native':true,
+        'new':true,
+        'return':true,
+        'var':true,
+	}
+});
