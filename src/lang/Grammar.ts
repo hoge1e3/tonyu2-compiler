2ec41b5385af03b9a05aa79f284dfdcaf7abe254
hoge1e3 <hoge1e3@gmail.com>
2022-02-24 12:28:05 +0900

    Grammar

--- src/lang/Grammar.ts
+++ src/lang/Grammar.ts
@@ -1,11 +1,9 @@
 import Parser from "./parser";
+const p=Parser;
 
 const Grammar=function () {
-	var p=Parser;
-
-	var $=null;
-	function trans(name) {
-		if (typeof name=="string") return $.get(name);
+	function trans(name:any) {
+		if (typeof name=="string") return get(name);
 		return name;
 	}
 	function tap(name) {
@@ -14,90 +12,103 @@ const Grammar=function () {
 			return st;
 		});
 	}
-	$=function (name){
-		var $$:any={};
-		$$.ands=function() {
-			var p=trans(arguments[0]);  //  ;
-			for (var i=1 ; i<arguments.length ;i++) {
-				p=p.and( trans(arguments[i]) );
-			}
-			p=p.tap(name);
-			$.defs[name]=p;
-			var $$$:any={};
-			$$$.autoNode=function () {
-				var res=p.ret(function () {
-					var res={type:name};
-					for (var i=0 ; i<arguments.length ;i++) {
-						var e=arguments[i];
-						var rg=Parser.setRange(e);
-						Parser.addRange(res, rg);
-						res["-element"+i]=e;
-					}
-					res.toString=function () {
-						return "("+this.type+")";
-					};
-				}).setName(name);
-				$.defs[name]=res;
-				return res;
-			};
-			$$$.ret=function (f) {
-				if (arguments.length==0) return p;
-				if (typeof f=="function") {
-					$.defs[name]=p.ret(f);
-					return $.defs[name];
-				}
-				var names=[];
-				var fn=function(e){return e;};
-				for (var i=0 ; i<arguments.length ;i++) {
-					if (typeof arguments[i]=="function") {
-						fn=arguments[i];
-						break;
-					}
-					names[i]=arguments[i];
-				}
-				var res=p.ret(function () {
-					var res={type:name};
-					res[Grammar.SUBELEMENTS]=[];
-					for (var i=0 ; i<arguments.length ;i++) {
-						var e=arguments[i];
-						var rg=Parser.setRange(e);
-						Parser.addRange(res, rg);
-						if (names[i]) {
-							res[names[i]]=e;
-						}
-						res[Grammar.SUBELEMENTS].push(e);
-					}
-					res.toString=function () {
-						return "("+this.type+")";
-					};
-					return fn(res);
-				}).setName(name);
-				$.defs[name]=res;
-				return  res;
-			};
-			return $$$;
-		};
-		$$.ors= function () {
-			var p=trans(arguments[0]);
-			for (var i=1 ; i<arguments.length ;i++) {
-				p=p.or( trans(arguments[i]) );
-			}
-			$.defs[name]=p.setName(name);
-			return $.defs[name];
-		};
-		return $$;
-	};
-
-	$.defs={};
-	$.get=function (name) {
-		if ($.defs[name]) return $.defs[name];
+	function comp<T1,T2>(o1:T1, o2:T2):T1&T2 {
+		return Object.assign(o1,o2);
+	}
+	function get(name:string) {
+		if (defs[name]) return defs[name];
 		return p.lazy(function () {
-			var r=$.defs[name];
+			var r=defs[name];
 			if (!r) throw "grammar named '"+name +"' is undefined";
 			return r;
 		}).setName("(Lazy of "+name+")");
-	};
-	return $;
+	}
+	function chain<P>(parsers:P[], f:(p:P, e:P)=>P) {
+		const [first,...rest]=parsers;
+		let p=first;
+		for (const e of rest) {
+			p=f(p,e);
+		}
+		return p;
+	}
+	const defs={};
+	return comp((name:string)=>{
+		return {
+			ands(...parsers) {
+				parsers=parsers.map(trans);
+				const p=chain(parsers, (p,e)=>p.and(e)).tap(name);
+				p.parsers=parsers;
+				/*let p=trans(first);
+				for (const e of rest) {
+					p=p.and( trans(e) );
+				}
+				p=p.tap(name);*/
+				defs[name]=p;
+				return {
+					autoNode() {
+						var res=p.ret(function (...args) {
+							const res={type:name};
+							for (var i=0 ; i<args.length ;i++) {
+								var e=args[i];
+								var rg=Parser.setRange(e);
+								Parser.addRange(res, rg);
+								res["-element"+i]=e;
+							}
+							res.toString=function () {
+								return "("+this.type+")";
+							};
+						}).setName(name);
+						defs[name]=res;
+						return res;
+					},
+					ret (...args) {
+						if (args.length==0) return p;
+						if (typeof args[0]=="function") {
+							defs[name]=p.ret(args[0]);
+							return defs[name];
+						}
+						const names=[];
+						let fn=(e:any)=>e;//(e){return e;};
+						for (var i=0 ; i<args.length ;i++) {
+							if (typeof args[i]=="function") {
+								fn=args[i];
+								break;
+							}
+							names[i]=args[i];
+						}
+						const res=p.ret(function (...args) {
+							var res={type:name};
+							res[Grammar.SUBELEMENTS]=[];
+							for (var i=0 ; i<args.length ;i++) {
+								var e=args[i];
+								var rg=Parser.setRange(e);
+								Parser.addRange(res, rg);
+								if (names[i]) {
+									res[names[i]]=e;
+								}
+								res[Grammar.SUBELEMENTS].push(e);
+							}
+							res.toString=function () {
+								return "("+this.type+")";
+							};
+							return fn(res);
+						}).setName(name);
+						defs[name]=res;
+						return  res;
+					}
+				};
+			},
+			ors(...parsers) {
+				parsers=parsers.map(trans);
+				const p=chain(parsers, (p,e)=>p.or(e)).tap(name);
+				p.parsers=parsers;
+				defs[name]=p;
+				return defs[name];
+			}
+		};
+		//return $$;
+	}, {defs,get});
+	//return $;
 };
 Grammar.SUBELEMENTS=Symbol("[SUBELEMENTS]");
 export=  Grammar;
