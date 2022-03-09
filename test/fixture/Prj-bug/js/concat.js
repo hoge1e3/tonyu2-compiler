5d2fe95b01696fd95427fde459f4964f4d0c2b1d
hoge1e3 <hoge1e3@gmail.com>
2022-03-09 12:02:05 +0900

    debugging in progress...

---
+++ test/fixture/Prj-bug/js/concat.js
@@ -0,0 +1,50 @@
+Tonyu.klass.define({
+  fullName: 'user.Chara',
+  shortName: 'Chara',
+  namespace: 'user',
+  includes: [],
+  methods: function (__superClass) {
+    return {
+      main :function _trc_Chara_main() {
+        "use strict";
+        var _this=this;
+        
+        _this.x=3;
+        while (true) {
+          Tonyu.checkLoop();
+          
+          
+        }
+      },
+      fiber$main :function _trc_Chara_f_main(_thread) {
+        "use strict";
+        var _this=this;
+        //var _arguments=Tonyu.A(arguments);
+        var __pc=0;
+        
+        _this.x=3;
+        
+        _thread.enter(function _trc_Chara_ent_main(_thread) {
+          if (_thread.lastEx) __pc=_thread.catchPC;
+          for(var __cnt=100 ; __cnt--;) {
+            switch (__pc) {
+            case 0:
+            case 1:
+              {
+                
+              }
+              __pc=1;break;
+            case 2     :
+              
+              _thread.exit(_this);return;
+            }
+          }
+        });
+      },
+      __dummy: false
+    };
+  },
+  decls: {"methods":{"main":{"nowait":false}},"fields":{"x":{}}}
+});
+
+//# sourceMappingURL=concat.js.map
